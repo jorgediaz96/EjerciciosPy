8255f86f577efb85a74e3d19c1e37fc777d44d87
jorgediaz96 <42659656+jorgediaz96@users.noreply.github.com>
2019-06-06 13:55:59 -0500

    correccion 4

--- ejercicios/4.py
+++ ejercicios/4.py
@@ -1,7 +1,10 @@
 from functools import reduce
-l=[16,22,44,23]
-p=list(map(lambda x:list(str(x)),l))
-n=list(filter(lambda j: reduce(lambda x,y: int(x)%2==0 and int(y)%2==0,j),p))
-n=list(map(lambda a:reduce(lambda c,b:c+b,a),n))
-n=list(map(lambda a:int(a),n))
-print(n)
+l=[[16,22,44,23],[10,20,40,50]]
+e=lambda x:list(str(x))
+e=list(map(lambda a:list(map(t,a)),e))
+e=list(map(lambda a:list(filter(lambda h:reduce(lambda j,n:int(j)%2==0 and int(n)%2==0),a)),e))
+e=list(filter(lambda a:a!=[],e))
+e=list(map(lambda a:list(map(lambda h:reduce(lambda j,n:j+n,h),a)),e))
+e=list(map(lambda a:list(map(lambda v:int(v),a)),e))
+e=list(map(lambda y:reduce(lambda a,b:a if a<b else b,y),e))
+print(e)
